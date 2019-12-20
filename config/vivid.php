7bfaae780bde008d3b772867c22c44e601c1117a
Meletios Flevarakis <m.flevarakis@acg.edu>
2019-12-20 23:19:25 +0200

    fixed typos inside vivid.php

--- config/vivid.php
+++ config/vivid.php
@@ -12,19 +12,19 @@ return [
     | You can also toggle which devices will be loaded by the application
     | by adding true of false to each device. For example:
     |
-    | Will be registred:
+    | Will be registered:
     | 'devices' => [
     |       'App\Devices\Example1\Providers\ExampleServiceProvider'
     |       'App\Devices\Example2\Providers\ExampleServiceProvider' => true
     | ]
     |
-    | Will not be registred:
+    | Will not be registered:
     | 'devices' => [
     |       'App\Devices\Example1\Providers\ExampleServiceProvider' => false
     | ]
     |
-    | TIP: You can also use env variables to controll how and when the devices
-    | will be loaded. For example you can disable certain devices on certain enviroments!
+    | TIP: You can also use env variables to control how and when the devices
+    | will be loaded. For example you can disable certain devices for certain enviroments!
     |
     */
     'devices' => [
