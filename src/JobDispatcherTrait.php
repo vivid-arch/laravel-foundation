41f76d87b9ff479e591901eccbc7741194fddce4
Meletis <fmeletis85@gmail.com>
2021-02-28 20:59:22 +0200

    Added strict type declaration to JobDispatcherTrait::job (string)

--- src/JobDispatcherTrait.php
+++ src/JobDispatcherTrait.php
@@ -25,13 +25,13 @@ trait JobDispatcherTrait
      * When the $arguments is an instance of Request
      * it will call dispatchFrom instead.
      *
-     * @param string                         $job
+     * @param string $job
      * @param array|\Illuminate\Http\Request $arguments
-     * @param array                          $extra
+     * @param array $extra
      *
      * @return mixed
      */
-    public function run($job, $arguments = [], $extra = [])
+    public function run(string $job, $arguments = [], $extra = [])
     {
         if ($arguments instanceof Request) {
             $result = $this->dispatch($this->marshal($job, $arguments, $extra));
